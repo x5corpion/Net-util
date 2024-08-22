c76d550df3456c03c975077954c76699cd47f1c5
x_5corpion <147299703+x5corpion@users.noreply.github.com>
2024-08-22 17:56:42 +0600

    Update ip finder.py

--- ip finder.py
+++ ip finder.py
@@ -1,11 +1,30 @@
 import socket
 import requests
+import getmac
+from colorama import Fore, Back, Style
+
+banner = f"""
+{Fore.GREEN}                                           
+  _  _     _          _   _ _ 
+ | \| |___| |_   _  _| |_(_) |
+ | .` / -_)  _| | || |  _| | |
+ |_|\_\___|\__|  \_,_|\__|_|_|    
+                    x_5corpion  
+
+>> [1] Find IP of any domain
+>> [2] My IP
+>> [3] My Mac addr
+>> [4] Exit
+{Style.RESET_ALL}
+"""
+
+print(banner)
 
 def get_ip(domain):
     try:
         socket.gethostbyname(domain)
-    except socket.gaierror:
-        print('Failed to get IP address for {}. (Invalid domain name?)'.format(domain))
+    except Exception as er:
+        print(er)
         exit()
     return socket.gethostbyname(domain)
 
@@ -16,37 +35,32 @@ def my_ip():
             print('Your public IP address is:', response.text)
         else:
             print('Failed to get public IP address. (Error {}: {})'.format(response.status_code, response.reason))
-    except requests.ConnectionError:
-        print('Failed to connect to the service.')
+    except Exception as err:
+        print(err)
+def get_mac_addr():
+    try:
+        mac_address = getmac.get_mac_address()
+        print(">>> Your mac addr is: {}".format(mac_address))
+    except Exception as g01tii:
+        print(g01tii)
 
 
-print("""
-             
-    ________     _____           __         
-   /  _/ __ \   / __(_)___  ____/ /__  _____
-   / // /_/ /  / /_/ / __ \/ __  / _ \/ ___/
- _/ // ____/  / __/ / / / / /_/ /  __/ /    
-/___/_/      /_/ /_/_/ /_/\__,_/\___/_/     
-                    Made by x_5corpion                              
+W4rF3: int = input(">>> Select tool: ")
+while True:
+    if W4rF3 == "1":
+        domain = input('>>> Enter the domain name: ')
+        print('>>> The IP address for {} is: {}'.format(domain, get_ip(domain)))
+        break
+    elif W4rF3 == "2":
+        my_ip()
+        break
+    elif W4rF3 == "3":
+        get_mac_addr()
+        break
+    elif W4rF3 == "4":
+        print(">>> Thanks")
+        break
+    else:
+        break
 
-""")
 
-print("""
-[1] Find ip of any domain
-[2] My IP
-[3] About
-[4] Exit
-""")
-
-prompt = input("Select tool: ")
-if prompt == "1":
-    domain = input('Enter the domain name: ')
-    print('The IP address for {} is: {}'.format(domain, get_ip(domain)))
-elif prompt == "2":
-    my_ip()
-elif prompt == "3":
-    print("This is a very basic tool for finding IP of any website and your IP. This is just for fun.For any queries: t.me/x_5corpion")
-elif prompt == "4":
-    exit
-
-print("Thanks for using my tool! \n:)")
